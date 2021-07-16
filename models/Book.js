4cc0f23b2f4f9aa6042bc3ec73842433ebeb54ef
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-07-16 10:47:26 -0500

    base

---
+++ models/Book.js
@@ -0,0 +1,28 @@
+// 1. IMPORTACIONES
+
+const mongoose          = require('mongoose');
+const Schema            = mongoose.Schema
+
+// 2. SCHEMA
+
+const bookSchema = new Schema(
+    {
+        tittle: String,
+        description: String,
+        author: String,
+        rating: Number
+    },
+    {
+        timestamps: true // Generar el moneto en el que se creó el documento
+    }
+);
+
+
+// 3. MODELO
+
+const Book = mongoose.model('Book', bookSchema);
+
+
+// 4. EXPORTACIÓN
+
+module.exports = Book
